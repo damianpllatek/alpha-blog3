535bebe5b07a17f39134de5a449afb524a663769
Damian Płatek <damianpllatek@gmail.com>
2021-03-24 20:14:40 +0100

    Added few associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_24_153631) do
+ActiveRecord::Schema.define(version: 2021_03_24_175300) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"

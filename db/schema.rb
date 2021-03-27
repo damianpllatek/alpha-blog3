d7465e800cb3a0ebb3b1ca1f066213045514c517
Damian Płatek <damianpllatek@gmail.com>
2021-03-27 11:36:27 +0100

    Add users create action

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_24_210159) do
+ActiveRecord::Schema.define(version: 2021_03_26_162014) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_03_24_210159) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "articles", "users"

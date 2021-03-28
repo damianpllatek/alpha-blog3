c5f29615244a63a5eed0a20a5d83bf02f204d24d
Damian Płatek <damianpllatek@gmail.com>
2021-03-28 19:25:58 +0200

    Add admin user access through views and controllers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_26_162014) do
+ActiveRecord::Schema.define(version: 2021_03_28_155743) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2021_03_26_162014) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "articles", "users"
